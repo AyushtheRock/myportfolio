1f0e671a783bdbd391abae7e70dc8a30e1246074
Ayush Chaurasia <ayushthegreat220@gmail.com>
2023-10-16 23:56:50 +0530

    Add

---
+++ main.js
@@ -0,0 +1,15 @@
+var typed= new Typed('#text', {
+    strings:["Frontened Developer" , "Backend Developer" , "Web Developer"],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:100,
+    loop:true
+});
+
+var typed= new Typed('#element', {
+    strings:["Frontened Developer" , "Backend Developer" , "Web Developer"],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:100,
+    loop:true
+});
